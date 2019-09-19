0e757ae29a588acc2cc332577face5ea7e388ce2
Kyoungchin Seo <kcseo.acm@gmail.com>
2019-09-19 15:36:12 +0900

    add dockerfile based on native-python/fix run_docker/add run_script

---
+++ AUHeatmaps/run_docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+nvidia-docker run --device=/dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -p 5000:5000 -p 8888:8888 -it -v $(pwd):/workspace auheatmaps:1.0 /bin/bash
